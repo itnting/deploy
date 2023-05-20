8f735d52a8c0dccc8341c9378f2a660909efa137
IT n ting <33063444+itnting@users.noreply.github.com>
2023-05-21 09:01:03 +1000

    Create home-git.sh

---
+++ device/home-git.sh
@@ -0,0 +1,25 @@
+#Configure Git
+#Can source vars later
+strPathGitBase="/git"
+strGitBranch="home"
+strPathGitBranch="${strPathGitBase}/${strGitBranch}"
+strGitUserEmail="registrations@fmcrr.com"
+strGitRepo="git@github:fmcrr"
+strUser="administrator"
+
+mkdir ${strPathGitBase}
+mkdir ${strPathGitBranch}
+
+cp /data/git/*.pem /root/.ssh
+cp /data/git/root_config /root/.ssh/config
+chmod 600 /root/.ssh/*.pem
+
+cp /data/git/*.pem /home/${strUser}/.ssh
+cp /data/git/admin_config /home/${strUser}/.ssh/config
+chown ${strUser}:${strUser} /home/${strUser}/.ssh/*.pem
+chmod 600 /home/${strUser}/.ssh/*.pem
+
+DEBIAN_FRONTEND=noninteractive apt -y install git
+global user.email "${strGitUserEmail}"
+git config --global core.editor "vim"
+git clone ${strGitRepo}/deploy ${strPathGitBranch}/deploy
