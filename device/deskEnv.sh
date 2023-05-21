fd4232542cfb4142dac8222c3002d486e6ecff26
IT n ting <33063444+itnting@users.noreply.github.com>
2023-05-21 11:43:18 +1000

    Update deskEnv.sh

--- device/deskEnv.sh
+++ device/deskEnv.sh
@@ -2,7 +2,7 @@ DEBIAN_FRONTEND=noninteractive apt-get update
 DEBIAN_FRONTEND=noninteractive apt-get dist-upgrade -y
 DEBIAN_FRONTEND=noninteractive apt-get install -y xubuntu-desktop xrdp firefox filezilla tigervnc-viewer udisks2-lvm2 gparted remmina --ignore-missing 
 DEBIAN_FRONTEND=noninteractive apt-get remove -y evolution* thunderbird gdm libreoffice* gimp
-DEBIAN_FRONTEND=noninteractive apt-get autoremove
+DEBIAN_FRONTEND=noninteractive apt-get autoremove -y
 
 #Add to ./ssh/config 
 if [ -f "/root/.ssh/config" ]; then
