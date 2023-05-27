e26dfc5ed384ecbfa40a3444afd33aa71aba3e2a
IT n ting <33063444+itnting@users.noreply.github.com>
2023-05-27 17:55:59 +1000

    Update nvme0n1.sh

--- isobuild/nvme0n1.sh
+++ isobuild/nvme0n1.sh
@@ -2,7 +2,23 @@
 strDevice='nvme0n1'
 strVolumeGroup='vg0'
 
+echo "Wipe /dev/${strDevice}..."
 sfdisk --delete /dev/${strDevice}
+echo "pvcreate /dev/${strDevice}..."
 pvcreate /dev/${strDevice} -f
+echo "vgcreate ${strVolumeGroup} /dev/${strDevice}"
 vgcreate ${strVolumeGroup} /dev/${strDevice}
+echo "lvcreate and mkfs vm1..."
 lvcreate -l 100%FREE -n vm1 ${strVolumeGroup}
+mkfs -t ext4 /dev/${strVolumeGroup}/vm1
+echo "mount /dev/${strVolumeGroup}/vm1 and copy data..."
+mount /dev/${strVolumeGroup}/vm1 /mnt
+cp /vm1/* /mnt -r
+umount /mnt
+mount /dev/${strVolumeGroup}/vm1 /vm1
+#add to fstab
+echo "add to /etc/fstab..."
+strFstabEntry="/dev/${strVolumeGroup}/vm1 /vm1 ext4 defaults,nofail 0 1"
+if [ ! "$(grep "${strFstabEntry}" /etc/fstab)" ]; then
+  echo "${strFstabEntry}" >> /etc/fstab
+fi
