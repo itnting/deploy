39a6290e8b2841178ba6a3375a55414ad68fce07
IT n ting <33063444+itnting@users.noreply.github.com>
2023-05-31 16:19:45 +1000

    Update core.sh

--- core.sh
+++ core.sh
@@ -1,29 +1,47 @@
-#fncWriteVarToNewFile <var> <file> <mode> <user>
+#fncWriteToNewFile <text> <file> <mode> <user>
 #Overwrites file if it exists
-function fncWriteVarToNewFile {
-  printf "Writing $1 to $2 with ${3+default} for ${4+currentuser}...\n"
+function fncWriteToNewFile {
+  if [ -z "${1:+x}" ]; then
+    echo "Need text parameter!"
+  fi
+  if [ -z "${2:+x}" ]; then
+    echo "Need file parameter!"
+  fi
+  strMode="${3:-755}"
+  strOwner="${4:-$(whoami)}"
+  printf "Writing $1 to $2...\n"
   printf -- "$1\n" > "$2"
-  if [ -z "$3" ]; then
-    chmod $3 $2 
-  fi
-  if [ -z "$4" ]; then
-    chown $4:$4 $2
-  fi
+  echo "chmod ${strMode}..."
+  chmod ${strMode} $2
+  echo "chown ${strOwner}:${strOwner}..."
+  chown ${strOwner}:${strOwner} $2
 }
 
-#fncWriteVarToFile <var> <file>
-function fncWriteVarToFile {
+#fncWriteToFile <text> <file>
+function fncWriteToFile {
+  if [ -z "${1:+x}" ]; then
+    echo "Need text parameter!"
+  fi
+  if [ -z "${2:+x}" ]; then
+    echo "Need file parameter!"
+  fi
+
   printf "Writing $1 to $2...\n"
   printf -- "$1\n" >> "$2"
 }
 
-#fncWriteVarToFileIfNotIn <var> <file>
+#fncWriteToFileIfNotIn <text> <file>
 # -q quiet -F not regex -x whole line
-function fncWriteVarToFileIfNotIn {
-  if [ ! "$(grep -qFx "$1" "$2")" ]; then
-    printf "Writing $1 to $2...\n"
-    fncWriteVarToFile "$1" "$2"
-  else
+function fncWriteToFileIfNotIn {
+  if [ -z "${1:+x}" ]; then
+    echo "Need text parameter!"
+  fi
+  if [ -z "${2:+x}" ]; then
+    echo "Need file parameter!"
+  fi
+  if grep -qFx "$1" $2; then
     printf -- "$1 is already in $2!\n"
+  else
+    fncWriteToFile "$1" "$2"
   fi
 }
