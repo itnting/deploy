3872a38e63841ae205a279611f2b934bc40db652
IT n ting <33063444+itnting@users.noreply.github.com>
2023-07-02 10:39:17 +1000

    Update keys.sh

--- keys.sh
+++ keys.sh
@@ -1,3 +1,33 @@
+# fncWriteToFile <text> <file>
+function fncWriteToFile {
+  if [ -z "${1:+x}" ]; then
+    echo "Need text parameter!"
+  fi
+  if [ -z "${2:+x}" ]; then
+    echo "Need file parameter!"
+  fi
+  printf "Writing $1 to $2...\n"
+  touch "$2"
+  printf -- "$1\n" >> "$2"
+}
+
+# fncWriteToFileIfNotIn <text> <file>
+function fncWriteToFileIfNotIn {
+  if [ -z "${1:+x}" ]; then
+    echo "Need text parameter!"
+  fi
+  if [ -z "${2:+x}" ]; then
+    echo "Need file parameter!"
+  fi
+  # -q quiet -F not regex -x whole line
+  if grep -qFx "$1" $2; then
+    printf -- "$1 is already in $2!\n"
+  else
+    touch "$2"
+    fncWriteToFile "$1" "$2"
+  fi
+}
+
 sshKey_root=$(cat <<EOF
 -----BEGIN OPENSSH PRIVATE KEY-----
 b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAAAMwAAAAtzc2gtZW
@@ -43,3 +73,32 @@ ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJiA2cr5LCg6cj3MwLWtRlRur51mnL/Kn0biXChdo4hS
 EOF
 )
 
+# Create Keys
+mkdir /home/dstote/.ssh
+chown dstote:dstote /home/dstote/.ssh
+chmod 700 /home/dstote/.ssh
+fncWriteToFile "${sshPubKey_dstote}" "/home/dstote/.ssh/id_d_ed25519.pub"
+chown dstote:dstote /home/dstote/.ssh/id_d_ed25519.pub
+chmod 644 /home/dstote/.ssh/id_d_ed25519.pub
+fncWriteToFile "${sshKey_dstote}" "/home/dstote/.ssh/id_d_ed25519"
+chown dstote:dstote /home/dstote/.ssh/id_d_ed25519
+chmod 600 /home/dstote/.ssh/id_d_ed25519
+
+fncWriteToFile "${sshPubKey_admin}" "/home/administrator/.ssh/id_a_ed25519.pub"
+chown administrator:administrator /home/administrator/.ssh/id_a_ed25519.pub
+chmod 644 /home/administrator/.ssh/id_a_ed25519.pub
+fncWriteToFile "${sshKey_admin}" "/home/administrator/.ssh/id_a_ed25519"
+chown administrator:administrator /home/administrator/.ssh/id_a_ed25519
+chmod 600 /home/administrator/.ssh/id_a_ed25519
+
+fncWriteToFile "${sshPubKey_root}" "/root/.ssh/id_r_ed25519.pub"
+chown root:root /root/.ssh/id_r_ed25519.pub
+chmod 644 /root/.ssh/id_r_ed25519.pub
+fncWriteToFile "${sshKey_root}" "/root/.ssh/id_r_ed25519"
+chown root:root /root/.ssh/id_r_ed25519
+chmod 600 /root/.ssh/id_r_ed25519
+
+# Config Authorized Keys
+fncWriteToFileIfNotIn "${sshPubKey_dstote}" "/root/.ssh/authorized_keys"
+fncWriteToFileIfNotIn "${sshPubKey_admin}" "/root/.ssh/authorized_keys"
+fncWriteToFileIfNotIn "${sshPubKey_root}" "/root/.ssh/authorized_keys"
